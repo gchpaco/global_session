cfdfeea90c6c55f78e091b58921232b75ce2fc63
Magne Land <magne@rightscale.com>
2013-09-27 16:29:23 -0700

    acu127822 remove the no-op rescue

--- lib/global_session.rb
+++ lib/global_session.rb
@@ -80,9 +80,5 @@ require 'global_session/directory'
 require 'global_session/encoding'
 require 'global_session/session'
 
-#Preemptively try to activate the Rails plugin, ignoring errors
-begin
-  require 'global_session/rails'
-rescue Exception => e
-  #no-op
-end
+# TODO: This should be done in the caller instead
+require 'global_session/rails'
